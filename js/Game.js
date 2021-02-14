95392e2bd6dca049e58a6908904499508c5af0e0
hifinerd <69434858+hifinerd@users.noreply.github.com>
2021-02-14 12:25:47 -0500

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -66,7 +66,11 @@ class Game{
 
             // Differentiate the main player by printing
             // the name of the player on the basket. 
-
+            if(index === player.index){
+                fill("black")
+                textSize(25)
+                text(allPlayers[plr].name ,x-25,y+25)
+            }
         }
 
 
@@ -81,8 +85,38 @@ class Game{
         }
 
         // Create and spawn fruits randomly
-
-        
+        if (frameCount %20 === 0){
+            fruits = createSprite(random(100,1000), 0, 100, 100)
+            fruits.velocityY = 6
+            var rand = Math.round(random(1,5))
+            switch(rand){
+            case 1: fruits. addImage("fruit1",fruit1_img)
+            break; 
+            case 2: fruits. addImage("fruit1",fruit2_img)
+            break;
+            case 3: fruits. addImage("fruit1",fruit3_img)
+            break;
+            case 4: fruits. addImage("fruit1",fruit4_img)
+            break; 
+            case 5: fruits. addImage("fruit1",fruit5_img)
+            break;
+            }
+            fruitGroup.add(fruits)
+        }
+        if (player.index !== null){
+            for (var i = 0; i <fruitGroup.length; i++) {
+                if (fruitGroup.get(i).isTouching(players)){
+                    fruitGroup.get(i).destroy()
+                    player.score=player.score+1
+                    player.update
+                }
+            }
+        }
+       
+        textSize(25)
+        fill(white)
+        text("Player 1:" +allPlayers.player.score,50,50)
+        text("Player 2:" +allPlayees.player2.score, 50, 100)
     }
 
     end(){
